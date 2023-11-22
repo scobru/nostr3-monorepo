bead6f796647ea2ae844a34a4369f31b65da3663
scobru <scobru1988@gmail.com>
2023-11-22 14:29:28 +0100

    update

--- packages/nextjs/index.js
+++ packages/nextjs/index.js
@@ -3,27 +3,25 @@ const dotenv = require("dotenv");
 const { matchFilters } = require("nostr-tools");
 const { WebSocketServer } = require("ws");
 const fs = require("fs");
+const path = require("path");
+
+const eventsFilePath = path.join(__dirname, "events.json");
 
 dotenv.config();
 
 const pid = Math.random().toString().slice(2, 8);
 const wss = new WebSocketServer({ port: process.env.PORT });
 const mogu = new Mogu(
-  process.env.APP_KEY,
-  process.env.PINATA_API_KEY,
-  process.env.PINATA_API_SECRET,
-  process.env.DB_NAME
+  process.env.NEXT_PUBLIC_APP_KEY,
+  process.env.NEXT_PUBLIC_PINATA_API_KEY,
+  process.env.NEXT_PUBLIC_PINATA_API_SECRET,
+  process.env.NEXT_PUBLIC_DB_NAME,
 );
 
 console.log("Running on port", process.env.PORT);
 
 // Check if cids.json exist
-let cid = null;
-let subs;
-
-// Check if cids.json exist
-subs = new Map();
-
+let subs = new Map();
 let connCount = 0;
 let events = [];
 
@@ -37,47 +35,10 @@ if (process.env.PURGE_INTERVAL) {
   }, process.env.PURGE_INTERVAL * 1000);
 }
 
-wss.on("connection", (socket) => {
+wss.on("connection", socket => {
   connCount += 1;
 
   console.log("Received connection", { pid, connCount });
-
-  if (fs.existsSync("cids.json")) {
-    // Read cids.json
-    cid = JSON.parse(fs.readFileSync("cids.json"));
-    // Load mogu from cid
-    let state;
-    console.log(cid);
-
-    const run = async () => {
-      const _state = await mogu.load(String(cid));
-      const json = JSON.stringify(_state);
-      return json;
-    };
-
-    run()
-      .then((state) => {
-        console.log("State", JSON.stringify(state));
-        // Qui puoi continuare a lavorare con 'state' dopo che la promessa è stata risolta
-        // create a Set() from state
-        subs = new Map(Object.entries(JSON.parse(state).content));
-        console.log("Subs", subs);
-      })
-      .catch((error) => {
-        console.error("Errore durante l'esecuzione di run():", error);
-      });
-
-    console.log("State", state);
-
-    // create a Set() from state
-    subs = new Map(Object.entries(JSON.parse(state).content));
-    console.log("Subs", subs);
-  } else {
-    // Create new subs
-    console.log("Creating new subs");
-    // Create new events
-  }
-
   const relay = new Instance(socket);
 
   if (process.env.PURGE_INTERVAL) {
@@ -85,18 +46,12 @@ wss.on("connection", (socket) => {
     relay.send([
       "NOTICE",
       "",
-      "Next purge in " +
-        Math.round(
-          (process.env.PURGE_INTERVAL * 1000 - (now - lastPurge)) / 1000
-        ) +
-        " seconds",
+      "Next purge in " + Math.round((process.env.PURGE_INTERVAL * 1000 - (now - lastPurge)) / 1000) + " seconds",
     ]);
   }
 
-  socket.on("message", (msg) => relay.handle(msg));
-  socket.on("error", (e) =>
-    console.error("Received error on client socket", e)
-  );
+  socket.on("message", msg => relay.handle(msg));
+  socket.on("error", e => console.error("Received error on client socket", e));
   socket.on("close", () => {
     relay.cleanup();
 
@@ -152,61 +107,87 @@ class Instance {
     }
   }
   onCLOSE(subId) {
-    //this.removeSub(subId);
-    //mogu.removeNode(subId);
-    const run = async () => {
-      cid = await mogu.store();
-    };
-    run();
-    // save cids on cids.json
-    fs.writeFileSync("cids.json", JSON.stringify(cid));
+    this.removeSub(subId);
   }
   onREQ(subId, ...filters) {
     console.log("REQ", subId, ...filters);
-
     this.addSub(subId, filters);
 
-    // converts _subs into a json string
-    const node = {
-      id: String(events.length), // Set an appropriate ID
-      type: "FILE",
-      name: String(subId),
-      parent: "",
-      children: [],
-      content: JSON.stringify(filters),
-      encrypted: false, // Set to true if data is encrypted
-    };
-
-    mogu.addNode(node);
-    console.log("Adding node:", node);
-
-    for (const event of events) {
+    const allEvents = readEventsFromFile();
+    allEvents.forEach(event => {
       if (matchFilters(filters, event)) {
-        console.log("match", subId, event);
-
         this.send(["EVENT", subId, event]);
-      } else {
-        console.log("miss", subId, event);
       }
-    }
+    });
 
     console.log("EOSE");
-
     this.send(["EOSE", subId]);
   }
   onEVENT(event) {
-    events.push(event);
-
+    writeEventToFile(event);
     console.log("EVENT", event, true);
-
     this.send(["OK", event.id, true]);
 
+    // Notifica a tutti i subscribers
     for (const [subId, { instance, filters }] of subs.entries()) {
       if (matchFilters(filters, event)) {
         console.log("match", subId, event);
-
         instance.send(["EVENT", subId, event]);
       }
     }
+  }
+}
+
+function readEventsFromFile() {
+  if (!fs.existsSync(eventsFilePath)) {
+    // Se il file non esiste, crealo con un array vuoto
+    fs.writeFileSync(eventsFilePath, JSON.stringify([], null, 2));
+    return [];
+  }
+
+  try {
+    const data = fs.readFileSync(eventsFilePath, "utf8");
+    const events = JSON.parse(data);
+
+    // Se il file è vuoto o non è un array, inizializzalo con un array vuoto
+    if (!Array.isArray(events)) {
+      return [];
+    }
+
+    return events;
+  } catch (e) {
+    console.error("Error reading events file:", e);
+    // In caso di errore di lettura o parsing, ritorna un array vuoto
+    return [];
+  }
+}
+
+function writeEventToFile(event) {
+  try {
+    const events = readEventsFromFile();
+
+    if (!Array.isArray(events)) {
+      console.error("Invalid events format in file");
+      return;
+    }
+
+    events.push(event);
+    fs.writeFileSync(eventsFilePath, JSON.stringify(events, null, 2));
+
+    // fetch events.json
+    const storedJson = fs.readFileSync(eventsFilePath, "utf8");
+    const storedEvents = JSON.parse(storedJson);
+
+    const node: EncryptedNode = {
+      id: String(wallet),
+      type: "FILE",
+      name: wallet,
+      parent: "",
+      children: [],
+      content: JSON.stringify({ salt, iv, ciphertext }),
+      encrypted: true,
+    };
+  } catch (e) {
+    console.error("Error writing event to file:", e);
   }
 }
